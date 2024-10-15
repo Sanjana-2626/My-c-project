c4119a36873b0c4beb3e50029642afd0567b883d
Sanjana-2626 <sanyash6342@gmail.com>
2024-10-15 16:41:32 +0530

    Add files via upload

---
+++ biggest number.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d;
+printf("enter the four numbers:");
+scanf("%d%d%d%d",&a,&b,&c,&d);
+if(a>b)
+   {
+   if(a>c)
+      {
+      if(a>d)
+      printf ("a is big");
+      else
+      ("printf d is big");
+      }
+   else
+      {
+      if(c>d)
+      printf ("c is big");
+      else
+      printf ("d is big");
+      }
+   }
+else
+   {
+   if(b>c)
+      {
+      if(b>d)
+      printf ("b is big");
+      else 
+      printf ("d is big");
+      }
+    else
+      {
+      if(c>d)
+      printf ("c is big");
+      else
+      printf ("d is big");
+      }
+    } 
+ }
